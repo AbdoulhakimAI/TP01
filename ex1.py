900b662d2faa8e3356678d156835e902ba1d6492
AbdoulhakimAI <abdoulhakim.abdillahi@gmail.com>
2024-09-11 16:25:32 -0400

    Exo 1

--- ex1.py
+++ ex1.py
@@ -1,5 +1,6 @@
 # TODO: Créer un script permettant le formattage du livre des records des JO.
 
 country = input("De quelle nationalité est l'athlète ? ")
-athlete = ...
-date = ...
+athlete = input("Quel est le nom de l'athlete ?")
+date = input("Quelle était la date du record ?")
+discipline = input("Quelle est la discipline ?")
