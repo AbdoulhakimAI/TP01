26d3e3b56b4522a359849d9074a401d9803a96da
lexi255 <alexakassar@hotmail.com>
2024-09-11 16:37:25 -0400

    exercice 1 1.0 alexa

--- ex1.py
+++ ex1.py
@@ -6,10 +6,13 @@ athlete = input("Quel est le nom de l'athlete ?")
 date = input("Quelle était la date du record ?")
 discipline = input("Quelle est la discipline ?")
 
-"""
+#exercice 1 alexa
+country = input("De quelle nationalité est l'athlète ? ")
 athlete = input("C'est quoi le nom de l'athlète?")
 date = input("Quelle est la date du record?")
 discipline= input("C'est quoi sa discipline?")
 catégorie= input("Dans quelle catégorie spécifiquement?")
 record= ("Quel est son record?")
-"""
+
+
+
