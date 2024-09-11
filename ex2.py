0b9171b587ce6eddac77a389a08813ceb36d3486
lexi255 <alexakassar@hotmail.com>
2024-09-11 17:11:35 -0400

    exercice 2 premier essai

--- ex2.py
+++ ex2.py
@@ -1,4 +1,13 @@
 # TODO: Créer un script pour calculer les ressources nécessaires pour assainir la Seine.
 # TODO: Importer les modules nécessaires.
 
-water_quantity = ...
+water_quantity =int(input ("Entrez la quantité d'eau à assainer:"))
+
+nbfiltreinitial=1
+nblampesintial=3
+qttechloreinitial=0.52
+
+print("le nombre de filtre utilisé est :", (water_quantity*nbfiltreinitial/5))
+print("Le nombre de lampes utilisées est:", (water_quantity*nblampesintial/5))
+print("La qunatité de chlore utilisée est: ",(water_quantity*qttechloreinitial/5))
+
