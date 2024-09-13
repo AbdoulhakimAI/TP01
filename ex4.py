0cb4e34500950c5f61c3851731a4a34e91d66eb9
lexi255 <alexakassar@hotmail.com>
2024-09-13 13:23:51 -0400

    exercice 4 1er essai

--- ex4.py
+++ ex4.py
@@ -3,4 +3,19 @@
 #        et affiche le résultat au format "XX km".
 #        Assurez une gestion du pourcentage valide au cours de votre programme (% toujours dans [0 ; 100]).
 
-battery_level = ...
+battery_level = int(input("Pourcentage de batterie:"))
+
+if 50<battery_level<= 100:
+    print("La distance possible est de", round(battery_level*2,1))
+
+if 25<battery_level<=50:
+    print("La distance possible est de", round(battery_level*0.5, 1))
+
+if 10<battery_level<=25:
+    print("La distance possible est de", round(battery_level*1, 1))
+
+if 5<battery_level<=10:
+    print("La distance possible est de", round(battery_level*2.5, 1))
+
+if 0<battery_level<=5:
+    print("La distance possible est de", round(battery_level*6, 1))
