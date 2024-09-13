1c7d06510b27b5f0a36bde04cb4c8fe1f06b095b
lexi255 <alexakassar@hotmail.com>
2024-09-13 13:01:39 -0400

    exercice 3 1er essai

--- ex3.py
+++ ex3.py
@@ -1,5 +1,10 @@
 # TODO: Calculer la distance qu'un poids peut atteindre en utilisant la formule de la portée d'un projectile.
 # TODO: Importer les modules nécessaires.
+import math
 
-speed = ... 
-angle = ...
+speed = float(input("Vitesse initiale (en m/s):"))
+angle = float(input("Angle de lancement (en degrés):"))
+gravitational_constant=9.8
+distance=((speed**2)*(math.sin(math.radians(2*angle))))/gravitational_constant
+
+print("La distance maximale en x est de", abs(round(distance,2)))
