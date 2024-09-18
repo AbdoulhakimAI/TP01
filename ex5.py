4d70dd578f25c1fceb6b7f272af2b86f1a1e8302
lexi255 <alexakassar@hotmail.com>
2024-09-18 16:07:18 -0400

    tous les exercice avecc formattage

--- ex5.py
+++ ex5.py
@@ -8,8 +8,7 @@ set1=set(code_medals)
 set2=set("G" +"S" +"B")
 
 if set1 == set2:
- print(f"""\n- {code_medals.count}("S") OR\n- {code_medals.count}("B") Argent\n- {code_medals.count}('G')Bronze\n""")
+ print(f"""(\n- {code_medals.count("G")}) Or\n- {code_medals.count("S")} Argent\n- {code_medals.count("B")} Bronze\n""")
 
-else: 
+else:      
     print("Ceci est une chaîne invalide. Veuillez entrer une chaîne valide")
-#github abdul
