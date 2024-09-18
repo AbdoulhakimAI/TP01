be29ee909b2d91a61884f755cd582b68cf3f46aa
lexi255 <alexakassar@hotmail.com>
2024-09-18 16:09:22 -0400

    tous les exercices avec formattage 1.0 a copier sur pycharm

--- ex1.py
+++ ex1.py
@@ -1,6 +1,5 @@
 # TODO: Créer un script permettant le formattage du livre des records des JO.
 
-<<<<<<< HEAD
 country = input("De quelle nationalité est l'athlète ?")
 athlete = input("Quel est son nom ?")
 date = input("Date du record ?")
@@ -11,17 +10,3 @@ record= input("Quel est le record ?")
 print(f"\nNouveau Record:\n--------------------\n{date} - {sport} - {category}:\n\t{athlete} ({country}) - {record}\n")
 
 
-=======
-#TODO: pour la categorie si c nul comment fait on pour fixer une valeur?
-
-
-#exercice 1 alexa
-country =str(input("De quelle nationalité est l'athlète ? "))
-athlete = str(input("C'est quoi le nom de l'athlète?"))
-date = str(input("Quelle est la date du record?"))
-sport= str(input("C'est quoi sa discipline?"))
-category= str(input("Dans quelle catégorie spécifiquement?"))
-record= str(input("Quel est son record?"))
-print(f"\nNouveau Record:\n--------------------\n {date} - {sport} - {category}:\n\t{athlete} ({country}) - {record}\n")
-        
->>>>>>> exercice5
